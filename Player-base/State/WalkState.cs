ea745ddbd4ebf6611e25660a4bc541cf182b67d0
Woon98K <61355325+koreko@users.noreply.github.com>
2021-04-03 21:44:44 +0900

    Fsm

---
+++ Player-base/State/WalkState.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WalkState : IState
+{
+    
+
+
+    public void OperateEnter()
+    {
+        
+    }
+
+    public void OperateExit()
+    {
+       
+    }
+
+    public void OperateUpdate()
+    {
+        Debug.Log("나걷고있음");
+    }
+}
